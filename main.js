0941c5c6cce1e4cb8e9fef5596da42a4b9c6f51c
Tito-74 <kipkirui133@gmail.com>
2021-07-24 16:59:50 +0300

    adding hide and show function

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#design').click(function(){
+        $('#design').hide();
+        $('.info1').show();
+    });
+    $('#develop').click(function(){
+        $('#develop').hide();
+        $('.info2').show();
+    });
+    $('#products').click(function(){
+        $('#products').hide();
+        $('.info3').show();
+    });
+    
+});
