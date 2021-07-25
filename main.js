8210fad00ca6cd891aad7ff7361b92a4129cf8ca
Tito-74 <kipkirui133@gmail.com>
2021-07-25 10:33:44 +0300

    adding on hover on images

--- main.js
+++ main.js
@@ -19,5 +19,29 @@ $(document).ready(function(){
         $('.card-1').css("opacity","100%");
         $("#card-text-1").hide(200);
     });
+    $('.card-2').mouseover(function(){
+        $('.card-2').css("opacity","80%");
+        $("#card-text-2").show();
+       
+    }).mouseout(function(){
+        $('.card-2').css("opacity","100%");
+        $("#card-text-2").hide(200);
+    });
+    $('.card-3').mouseover(function(){
+        $('.card-3').css("opacity","50%");
+        $("#card-text-3").show();
+       
+    }).mouseout(function(){
+        $('.card-3').css("opacity","100%");
+        $("#card-text-3").hide(200);
+    });
+    $('.card-4').mouseover(function(){
+        $('.card-4').css("opacity","50%");
+        $("#card-text-4").show();
+       
+    }).mouseout(function(){
+        $('.card-4').css("opacity","100%");
+        $("#card-text-4").hide(200);
+    });
 
 });
